73431ff935fbcc8a7520570cb26f33d793dd4dca
fuwei16 <fuwei16@centalin.com.cn>
2016-08-26 17:20:31 +0800

    add message

---
+++ Src/Core.Module/ContextCollectHandler.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Routing;
+using Core.Cache;
+using Core.Config;
+using DevTrends.MvcDonutCaching;
+using Framework.Utility;
+using Newtonsoft.Json;
+
+namespace Core.Module
+{
+    public abstract class ContextCollectHandler:IHttpHandler, IRouteHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            response.Clear();
+            response.ContentEncoding=Encoding.Default;
+
+            if (!string.Equals(Fetch.ServerDomain, "localhost", StringComparison.OrdinalIgnoreCase))
+            {
+                response.Write(" < h1 > 非法访问收集数据！</ h1 > ");
+            }
+            else
+            {
+                ProcessRequest(request,response);
+            }
+            response.Flush();
+            response.End();
+            response.Close();
+           
+        }
+        public abstract void ProcessRequest(HttpRequest req, HttpResponse res);
+        public bool IsReusable { get; }
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return this;
+        }
+    }
+
+    public class ConfigCollect:ContextCollectHandler
+    {
+        public override void ProcessRequest(HttpRequest req, HttpResponse res)
+        {
+            var type = typeof (LocalCachedConfigContext);
+            var configContext = LocalCachedConfigContext.Current;
+
+            if (string.IsNullOrEmpty(req["config"]))
+            {
+                res.Write("<p><h1>网站当前配置列表：</h1><p>");
+
+                foreach (var p in type.GetProperties())
+                {
+                    if (p.Name != "ConfigService")
+                        res.Write(string.Format("<p><a href='?config={0}' target='_blank'>{0} [点击查看]</a></p>", p.Name));
+                }
+            }
+            else
+            {
+                foreach (var p in type.GetProperties())
+                {
+                    if (p.Name == req["config"] && p.Name != "DaoConfig")
+                    {
+                        var curreConfig = p.GetValue(configContext, null);
+                        if (configContext != null)
+                        {
+                            res.ContentType = "text/xml";
+                            res.ContentEncoding=Encoding.Default;
+                            res.Write(SerializationHelper.XmlSerialize(curreConfig));
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    public class CacheCollect : ContextCollectHandler
+    {
+        public override void ProcessRequest(HttpRequest req, HttpResponse res)
+        {
+            if (req.QueryString.Count == 0)
+            {
+                res.Write("<p><h1>当前网站缓存列表</h1></p>");
+                var cacheItemList=new List<string>();
+                var s = "<a href='?cacheClear=true' target='_blank'>[!点击清除所有缓存]</a>";
+                cacheItemList.Add(s);
+
+                var cacheEnumerator = HttpRuntime.Cache.GetEnumerator();
+                while (cacheEnumerator.MoveNext())
+                {
+                    var key = cacheEnumerator.Key.ToString();
+                    s = string.Format("<b>{0}</b>:(<a href='?key={0}' target='_blank'>查看数据</a>)", key);
+                    cacheItemList.Add(s);
+
+                }
+                cacheItemList.Sort();
+                res.Write(string.Join("<hr>",cacheItemList));
+
+            }
+            else
+            {
+                if (req["cacheClear"] != null)
+                {
+                    CacheContext.Clear();
+                    var cacheManager = new OutputCacheManager();//？？？清除缓存这个为什么？？？
+                    cacheManager.RemoveItems();
+
+                    res.Write("清除缓存成功！<a href=''>[!点击返回]</a>");
+                }
+                if (req["key"] != null)
+                {
+                    var data = CacheContext.Get(req["key"]);
+                    if (data != null)
+                    {
+                        res.Write(JsonConvert.SerializeObject(data)+ "&nbsp<a href=''>[!点击返回]</a>");
+                    }
+                }
+            }
+        }
+
+        
+    }
+
+    public class LogCollect:ContextCollectHandler
+    {
+        public override void ProcessRequest(HttpRequest req, HttpResponse res)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/Core.Module/HttpModuleService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Core.Module
+{
+    public class HttpModuleService : IHttpModule
+    {
+        private static object _moduleStart = new object();
+        private static bool _isStarted = false;
+        private static Dictionary<string, ContextCollectHandler> handlers;
+        public void Init(HttpApplication context)
+        {
+            if (!_isStarted)
+                lock (_moduleStart)
+                {
+                    if (!_isStarted)
+                    {
+                        _isStarted = true;
+                        this.InitHandlers();
+                    }
+                }
+            context.BeginRequest += Context_BeginRequest;
+        }
+
+        private void Context_BeginRequest(object sender, System.EventArgs e)
+        {
+            var context = HttpContext.Current;
+            var path = context.Request.CurrentExecutionFilePath;
+
+            if (!path.EndsWith("collect.axd", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+            path = path.Substring(path.IndexOf("/", StringComparison.Ordinal) + 1);
+            var handler = path.Substring(0, path.LastIndexOf(".", StringComparison.Ordinal)).ToLower();
+            if (handlers.ContainsKey(path))
+            {
+                handlers[handler].ProcessRequest(context);
+            }
+
+        }
+
+        public void Dispose()
+        {
+
+
+        }
+
+        private void InitHandlers()
+        {
+            if (handlers == null)
+            {
+                handlers=new Dictionary<string, ContextCollectHandler>();
+            }
+
+            var handlerTypes =
+                this.GetType().Assembly.GetTypes().Where(r => r.BaseType == typeof (ContextCollectHandler));
+            foreach (var handler in handlerTypes)
+            {
+                if(handlers.ContainsKey(handler.Name))continue;
+                var instance = Activator.CreateInstance(handler) as ContextCollectHandler;
+                if (instance != null)
+                {
+                    handlers.Add(handler.Name.ToLower(),instance);
+                }
+            }
+        }
+    }
+}
