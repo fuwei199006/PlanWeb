6d47fa6e83e5664b5f88fee3b5b4a7b62d46e702
fuwei16 <fuwei16@centalin.com.cn>
2016-09-01 18:28:03 +0800

    fix

--- Src/Plain.UI/Controllers/LoginController.cs
+++ Src/Plain.UI/Controllers/LoginController.cs
@@ -21,7 +21,7 @@ namespace Plain.UI.Controllers
         private readonly ILoginService _loginService;
         private readonly IRegisterService _registerService;
         private readonly IUserService _userService;
-        
+
         public LoginController(ILoginService loginService, IRegisterService registerService, IUserService userService)
         {
             _loginService = loginService;
@@ -42,7 +42,7 @@ namespace Plain.UI.Controllers
                 return View();
             }
             var register = _registerService.GetRegisterByToken(token);
-            if (register==null)
+            if (register == null)
             {
                 //跳转到错误页面，已经过期
                 return SkipAndAlert("注册信息不存在，请先注册,3秒后自动跳转注册页面", MsgType.Error, true, Url.Action("Register"));
@@ -50,14 +50,14 @@ namespace Plain.UI.Controllers
             else if (register.Expiretime < DateTime.Now)
             {
                 //跳转到错误页面，已经过期
-                return SkipAndAlert("注册信息已经过期，请重新注册,3秒后自动跳转注册页面", MsgType.Error,true,Url.Action("Register"));
+                return SkipAndAlert("注册信息已经过期，请重新注册,3秒后自动跳转注册页面", MsgType.Error, true, Url.Action("Register"));
             }
             //如果没有过期则，生成用户和登录信息
             var result = _userService.ActiveUserByEmail(register.RegisterEmail);
             if (result != null)
             {
                 _registerService.DeleteRegister(token);
-                return SkipAndAlert("注册成功,欢迎您的加入!",MsgType.Success,true,Url.Action("Index","Home"));
+                return SkipAndAlert("注册成功,欢迎您的加入!", MsgType.Success, true, Url.Action("Index", "Home"));
             }
             return SkipAndAlert("系统出错，先休息一下吧！", MsgType.Error);
         }
@@ -65,13 +65,13 @@ namespace Plain.UI.Controllers
         [HttpPost]
         public ActionResult Register(Basic_Register register)
         {
-            register.CreateTime=DateTime.Now;
+            register.CreateTime = DateTime.Now;
             register.Expiretime = DateTime.Now.AddDays(7);
             register.RetisterIp = Fetch.UserIp;
             register.RegisterDevice = RequestHelper.GetDeviceJson(Request.UserAgent);
             register.RegisterPassword = MD5Encrypt.Md5(register.RegisterPassword);
             register.RegisterConfirmPassword = register.RegisterPassword;
-            register.RegisterTime=DateTime.Now;
+            register.RegisterTime = DateTime.Now;
             register.RegisterStatus = true;
             register.RegisterPhone = "NaN";//代表没有手机号
             register.RegisterToken = Guid.NewGuid();
@@ -86,36 +86,32 @@ namespace Plain.UI.Controllers
                 RegisterTime = register.RegisterTime,
                 ModifyTime = DateTime.Now,
                 CreateTime = DateTime.Now,
-                UserStaus =0
+                UserStaus = 0
             };
-         
-            try
-            {
-                var result = _registerService.AddRegister(register);
-                _userService.AddUser(user);
-                var sentRes=MailContext.SendEmail(result.RegisterEmail, "Plain平台注册", @"<meta charset='utf-8'/><body><p>Plain 模板测试 </p><p> 点击下面的链接完成注册："+Request.Url+"?token="+ result.RegisterToken.ToString()+"</p></body> ");
-                if (sentRes)
-                {
-                    return SkipAndAlert("注册成功,系统已经将激活邮件发送到您的邮箱，请查收！", MsgType.Success);
-                }
 
-                return SkipAndAlert("系统出错，先休息一下吧！", MsgType.Error);
+
+
+            var result = _registerService.AddRegister(register);
+            _userService.AddUser(user);
+            var sentRes = MailContext.SendEmail(result.RegisterEmail, "Plain平台注册", @"<meta charset='utf-8'/><body><p>Plain 模板测试 </p><p> 点击下面的链接完成注册：" + Request.Url + "?token=" + result.RegisterToken.ToString() + "</p></body> ");
+            if (sentRes)
+            {
+                return SkipAndAlert("注册成功,系统已经将激活邮件发送到您的邮箱，请查收！", MsgType.Success);
             }
-            catch (DbEntityValidationException e)
-            {
 
-                return SkipAndAlert("系统出错，先休息一下吧！", MsgType.Error);
-            } 
-           
+            return SkipAndAlert("系统出错，先休息一下吧！", MsgType.Error);
+
+
+
         }
 
 
-        
+
         [HttpGet]
         public JsonResult ValideUser(string RegisterEmail)
         {
             var result = _userService.EmailExist(RegisterEmail);
-            if (result == null) return Json(true, JsonRequestBehavior.AllowGet); 
+            if (result == null) return Json(true, JsonRequestBehavior.AllowGet);
             return Json(false, JsonRequestBehavior.AllowGet);
 
         }
