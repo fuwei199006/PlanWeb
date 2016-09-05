180d2a001e1adc21e6912f353ac259f526625171
fuwei199006 <756091180@qq.com>
2016-09-05 23:13:04 +0800

    add area

--- Src/Plain.CMS/Controllers/HomeController.cs
+++ Src/Plain.CMS/Controllers/HomeController.cs
@@ -3,28 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 
 namespace Plain.CMS.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Plain.Web.ControllerBase
     {
-        public ActionResult Index()
+        public ActionResult Get()
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+}
 }
