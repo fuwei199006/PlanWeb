68dac0416693494428e8f9ffbf9463c256017cef
fuwei199006 <756091180@qq.com>
2016-09-27 23:29:03 +0800

    add TableProvider

--- Src/Framework.Extention/ConverterExtention.cs
+++ Src/Framework.Extention/ConverterExtention.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
 using System.Text;
 
 namespace Framework.Extention
@@ -181,6 +184,47 @@ namespace Framework.Extention
             else
                 return defalut;
         }
+
+        /// <summary>
+        /// DataTable转换成泛型List
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static IList<T> ToList<T>(this DataTable dt)
+        {
+            // 定义集合    
+            IList<T> ts = new List<T>();
+
+            // 获得此模型的类型   
+            Type type = typeof(T);
+            string tempName = "";
+
+            T t = default(T);
+            foreach (DataRow dr in dt.Rows)
+            {
+                t = Activator.CreateInstance<T>();
+                //t = new T();
+                // 获得此模型的公共属性      
+                PropertyInfo[] propertys = t.GetType().GetProperties();
+                foreach (PropertyInfo pi in propertys)
+                {
+                    tempName = pi.Name;  // 检查DataTable是否包含此列    
+
+                    if (dt.Columns.Contains(tempName))
+                    {
+                        // 判断此属性是否有Setter      
+                        if (!pi.CanWrite) continue;
+
+                        object value = dr[tempName];
+                        if (value != DBNull.Value)
+                            pi.SetValue(t, value, null);
+                    }
+                }
+                ts.Add(t);
+            }
+            return ts;
+        }
+ 
 
     }
 }
---
+++ Src/Tool.T4Templent/ServiceAndDto/ModelProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Core.Config;
+using Framework.Extention;
+
+namespace Tool.T4Templent.ServiceAndDto
+{
+    public class ModelProvider
+    {
+        public static MssqlDbHelper SqlDbHelper = null;
+
+        static ModelProvider()
+        {
+            SqlDbHelper.conStr = LocalCachedConfigContext.Current.DaoConfig.BussinessDaoConfig;
+
+        }
+        public static List<string> GetTable()
+        {
+            var sql = @"SELECT Name FROM  PlanDB..SysObjects Where XType='U'  ";
+            var dbTable = SqlDbHelper.ExecReturnDataSet(sql).Tables[0];
+            return dbTable.Select().Select(x => x["Name"].ToString()).ToList();
+        }
+
+
+        public static List<Filed> GetFiledByTable(string tableName)
+        {
+            var sql = @"    SELECT    col.name AS Name ,
+                                    type.name AS Type,
+			                        col.length AS Length,
+			                        col.isnullable AS IsNullAble
+                          FROM      sys.syscolumns col
+                                    JOIN sys.systypes type ON type.xtype = col.xtype
+                          WHERE     id = OBJECT_ID('Basic_LoginInfo')
+                          ORDER BY  col.colorder";
+            var dbFiled = SqlDbHelper.ExecReturnDataSet(sql).Tables[0];
+            return dbFiled.ToList<Filed>().ToList();
+
+        }
+
+      
+    }
+
+
+    public class Filed
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Length { get; set; }
+
+        public int IsNullable { get; set; }
+    }
+}
