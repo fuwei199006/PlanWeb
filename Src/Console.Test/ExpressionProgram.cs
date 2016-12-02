d6ce420515197f894a7ebe2eaf6f2944dd22ec69
fuwei16 <fuwei16@centalin.com.cn>
2016-12-02 11:52:18 +0800

    fix

--- Src/Console.Test/ExpressionProgram.cs
+++ Src/Console.Test/ExpressionProgram.cs
@@ -1,16 +1,44 @@
-﻿using System.Linq.Expressions;
- 
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
 namespace PConsole.Test
 {
     public class ExpressionProgram
     {
-        public static void _Main(string[] args)
+        static void Main(string[] args)
         {
-            Expression firstArg = Expression.Constant(2);
-            Expression secondArg = Expression.Constant(3);
-            Expression add = Expression.Add(firstArg, secondArg);
-            System.Console.WriteLine(add);
-            System.Console.ReadKey();
+            System.Diagnostics.Process p = Process.GetCurrentProcess();
+            p.ProcessorAffinity = (IntPtr)0x0001;
+
+            const int SAMPLING_COUNT = 200;
+            const double PI = 3.14159;
+            const int TOTAL_AMPLITUDE = 250; // the length of each time piece 
+                                             //const int system_busy = 10; // take the system cpu consume into consideration 
+
+            double[] busySpan = new double[SAMPLING_COUNT];
+            int amplitude = (TOTAL_AMPLITUDE) / 2;
+            double radian = 0.0;
+            double radianIncreament = 2.0 / (double)SAMPLING_COUNT;
+
+            for (int i = 0; i < SAMPLING_COUNT; i++)
+            {
+                busySpan[i] = (double)(amplitude + Math.Sin(PI * radian) * amplitude);
+                radian += radianIncreament;
+            }
+
+            int startTick = Environment.TickCount;
+
+            for (int j = 0; ; j = (j + 1) % SAMPLING_COUNT)
+            {
+                startTick = Environment.TickCount;
+                while ((Environment.TickCount - startTick) < busySpan[j])
+                {
+                    // 
+                }
+                System.Threading.Thread.Sleep(TOTAL_AMPLITUDE - (int)busySpan[j]);
+            }
         }
     }
+
 }
--- Src/Console.Test/MemcahePrograme.cs
+++ Src/Console.Test/MemcahePrograme.cs
@@ -7,9 +7,9 @@ namespace PConsole.Test
 {
     public class MemcahePrograme
     {
-      
-            public static void Main(string[] args)
-            {
+
+        public static void _Main(string[] args)
+        {
             //参数设置
             string SockIOPoolName = "Test_SockIOPoolName";
             string[] MemcacheServiceList = { "127.0.0.1:11211" };
@@ -22,32 +22,36 @@ namespace PConsole.Test
             //实例化Client
             MemcachedClient MClient = new MemcachedClient();
             MClient.PoolName = SockIOPoolName;
-            
-            Console.WriteLine("1.创建memcache缓存Hello World");
-            MClient.Add("Key1001", "Hello World");
-            Console.WriteLine("2.查询缓存信息{0}", MClient.Get("Key1001"));
 
-            Console.WriteLine("3.修改memcache缓存Hello World");
-            MClient.Set("Key1001", "Hello World - 修改版");
-            Console.WriteLine("4.查询缓存信息{0}", MClient.Get("Key1001"));
-
-            if (MClient.KeyExists("Key1001"))
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("5.删除memcache缓存");
-                MClient.Delete("Key1001");
-            }
+                Console.WriteLine("1.创建memcache缓存Hello World");
+                MClient.Add("Key1001", "Hello World");
+                Console.WriteLine("2.查询缓存信息{0}", MClient.Get("Key1001"));
 
-            if (MClient.KeyExists("Key1001"))
-                Console.WriteLine(MClient.Get("Key1001"));
-            else
-                Console.WriteLine("6.删除已删除");
+                Console.WriteLine("3.修改memcache缓存Hello World");
+                MClient.Set("Key1001", "Hello World - 修改版");
+                Console.WriteLine("4.查询缓存信息{0}", MClient.Get("Key1001"));
 
-            Student stud = new Student() { id = "10001", name = "张三" };
-            MClient.Add("student", stud);
-            Student getStud = MClient.Get("student") as Student ?? new Student();
+                if (MClient.KeyExists("Key1001"))
+                {
+                    Console.WriteLine("5.删除memcache缓存");
+                    MClient.Delete("Key1001");
+                }
+
+                if (MClient.KeyExists("Key1001"))
+                    Console.WriteLine(MClient.Get("Key1001"));
+                else
+                    Console.WriteLine("6.删除已删除");
+
+                Student stud = new Student() { id = "10001", name = "张三" };
+                MClient.Add("student", stud);
+                Student getStud = MClient.Get("student") as Student ?? new Student();
                 Console.WriteLine("6.缓存实体对象：{0} {1}", getStud.id, getStud.name);
 
-            MClient.Add("Key1002", "我已设置过期时间1分钟", DateTime.Now.AddMinutes(1));
+                MClient.Add("Key1001", "我已设置过期时间1分钟", DateTime.Now.AddMinutes(1));
+            }
+
             //while (true)
             //{
             //    if (MClient.KeyExists("Key1002"))
@@ -62,9 +66,9 @@ namespace PConsole.Test
             //    }
             //}
 
-                Console.ReadKey();
-            }
-         
+            Console.ReadKey();
+        }
+
     }
 
     [Serializable]
