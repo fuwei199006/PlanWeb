4845b1e95cc6b4d817b03003d63d052e6a3fa299
fuwei16 <fuwei16@centalin.com.cn>
2016-12-02 09:36:31 +0800

    add file

---
+++ Src/Core.Cache/RadiusProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Core.Cache
+{
+    public class RadiusProvider : ICacheProvider
+    {
+        public object Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear(string regKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, object value, int min)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, object value, int minutes, bool isAbsoluteExpiration, Action<string, object, string> onRemove)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
