63c0f3ad0d135db2df1b3153dccf74644ef7ba5c
fuwei16 <fuwei16@centalin.com.cn>
2016-12-21 17:51:45 +0800

    fix avalon

--- Src/Framework.Extention/StringExtention.cs
+++ Src/Framework.Extention/StringExtention.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Framework.Utility;
-
-namespace Framework.Extention
+﻿namespace Framework.Extention
 {
-   public static class StringExtention
+    public static class StringExtention
     {
         public static string CutString(this string str,int len)
         {
--- Src/Plain.Web/CmsArticleCache.cs
+++ Src/Plain.Web/CmsArticleCache.cs
@@ -15,32 +15,34 @@ namespace Plain.Web
         {
             get
             {
+#if DEBUG
+                Current.Clear();
+#endif
                 return CacheContext.Get(CacheKey.ArticleList,
                      () => ServiceContext.Current.CreateService<IArticleService>().GetArticles());
             }
         }
-       public  void Clear()
+        public void Clear()
         {
-            var keyRegex = BaseKey.CMS_ + "*";
+            var keyRegex = BaseKey.CMS_ + ".+";
             CacheContext.Clear(keyRegex);
         }
         public static CmsArticleCache Current
         {
             get
             {
-#if DEBUG
-                 var cache= CacheContext.GetItem<CmsArticleCache>();
-                cache.Clear();
-                return cache;
-#else 
+
                 return CacheContext.GetItem<CmsArticleCache>();
-#endif
+
             }
         }
         public List<Basic_Article> this[string key]
         {
             get
             {
+//#if DEBUG
+//                Current.Clear();
+//#endif
                 return CacheContext.Get(key,
                      () => Articles.Where(r => r.Position == key).ToList());
             }
