7b99a68fe8f62e29060887fdfd3906f7fb396344
fuwei16 <fuwei16@centalin.com.cn>
2016-12-30 18:37:40 +0800

    add spider

---
+++ Src/Tool.ArticleSpider/Dtos/SinaBlogKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool.ArticleSpider.Dtos
+{
+   public  class SinaBlogKey
+    {
+        public const string UrlRegex = @"http://blog\.[A-Za-z0-9%\-_@]+\.[A-Za-z0-9%\-_@]+[A-Za-z0-9\.\/=\?%\-&_~`@:\+!;]*(\.html|\.html)\?.+";
+        //public const string UrlRegex = @"http://blog\.[A-Za-z0-9%\-_@]+\.[A-Za-z0-9%\-_@]+[A-Za-z0-9\.\/=\?%\-&_~`@:\+!;]*(\.html|\.html)\?.+";
+    }
+}
