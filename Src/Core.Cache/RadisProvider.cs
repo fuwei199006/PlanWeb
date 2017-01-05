cb16c79ce2963c97ca28908e06baf9dab838719f
fuwei16 <fuwei16@centalin.com.cn>
2017-01-05 10:20:58 +0800

    fix

---
+++ Src/Core.Cache/RadisProvider.cs
@@ -0,0 +1,44 @@
+﻿using Core.Config;
+using Core.Exception;
+using Framework.Utility.Extention;
+using System;
+using System.Linq;
+
+namespace Core.Cache
+{
+    public class RadisProvider : ICacheProvider
+    {
+
+        public RadisProvider()
+        {
+            var cacheProviderItem = LocalCachedConfigContext.Current.CacheConfig.CacheProviderItems.FirstOrDefault(r => r.Type.TrimAll() == (this.GetType().FullName.ToString() + "," + this.GetType().Namespace));
+            if (cacheProviderItem == null)
+            {
+                throw new ConfigInvalidException("配置错误:可能是使用的分布式缓存,但配置文件没有正确的配置成分布式的Provider");
+            }
+
+            RadisUtil.IP = cacheProviderItem.Ips;
+            RadisUtil.Port = cacheProviderItem.Port;
+
+        }
+        public object Get(string key)
+        {
+            return RadisUtil.Get(key);
+        }
+
+        public void Set(string key, object value)
+        {
+            RadisUtil.Set(key, value);
+        }
+
+        public void Remove(string key)
+        {
+            RadisUtil.Remove(key);
+        }
+
+        public void Set(string key, object value, int min)
+        {
+            RadisUtil.Set(key, value, min);
+        }
+    }
+}
---
+++ Src/Core.Cache/RadisUtil.cs
@@ -0,0 +1,51 @@
+﻿using Core.Config;
+using ServiceStack.Redis;
+using System;
+
+namespace Core.Cache
+{
+    public class RadisUtil
+    {
+
+        public static string IP { get; set; }
+        public static int Port { get; set; }
+        private static RedisClient _redisClient;
+       public static RedisClient RedisClient
+        {
+            get
+            {
+                if (_redisClient == null)
+                {
+                    _redisClient= new RedisClient(IP, Port);
+                }
+                return _redisClient;
+            }
+        }
+
+
+        public static object Get(string key)
+        {
+            return RedisClient.Get(key);
+        }
+
+
+
+        public static void Remove(string key)
+        {
+            RedisClient.Delete(key);
+        }
+
+
+
+        public static void Set(string key, object value, int min)
+        {
+            RedisClient.Set(key, value, DateTime.Now.AddMinutes(min));
+        }
+        public static void Set(string key, object value)
+        {
+            Set(key, value, LocalCachedConfigContext.Current.SystemConfig.CacheExpiteTime);
+        }
+
+
+    }
+}
