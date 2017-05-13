60f3d8470c1b0ba47b14d6ed1e0982bce6b8b49a
fuwei <756091180@qq.com>
2017-05-13 23:08:45 +0800

    add task

---
+++ Src/Plain.TaskUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plain.TaskUI.Startup))]
+namespace Plain.TaskUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
