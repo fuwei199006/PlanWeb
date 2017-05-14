15d4353be8b40b7b8e1a0ebe98dad6833235da34
fuwei <756091180@qq.com>
2017-05-14 21:56:05 +0800

    fix

---
+++ Src/Tools.NodeService/BaseMonitor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.Log;
+
+namespace Tools.NodeService
+{
+    public abstract class BaseMonitor
+    {
+        public abstract void Run();
+
+        public abstract int Interval { get; set; }
+
+        public BaseMonitor()
+        {
+            Task.Factory.StartNew(TryRun);
+        }
+
+        public void TryRun()
+        {
+            while (true)
+            {
+                try
+                {
+                    Run();
+                    Thread.Sleep(Interval);
+                }
+                catch (Exception exp)
+                {
+
+                    Log4NetHelper.Error(LoggerType.ServiceExceptionLog, this.GetType().Name + "出现严重错误", exp);
+                }
+
+            }
+
+        }
+    }
+}
