2031e0159ffff4f08e65485ab188983d0bf4fbce
fuwei <w_fu@Ctrip.com>
2017-05-15 14:45:38 +0800

    add

---
+++ Src/Plain.Log/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plain.Log.Startup))]
+namespace Plain.Log
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
